71fd2f48c1b0db221882fa69d4115073322b6d2b
npronin <noxywille@yandex.ru>
2025-02-25 15:15:08 +0300

    added returnMessage

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,10 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, qa!");
+
+        System.out.println(returnMessage());
+    }
+    private static String returnMessage(){
+        return "Hello, qa!";
     }
 }
