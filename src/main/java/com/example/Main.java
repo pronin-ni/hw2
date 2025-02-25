9481179b3bafec80e40dfa6313c31be54e85a617
Nikita <47008746+pronin-ni@users.noreply.github.com>
2025-02-25 15:54:34 +0300

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,9 +3,10 @@ package com.example;
 public class Main {
     private static String msg = "Hello, qa!!!!!";
 
+    private static String msg1 = "Hello, qa!!!!!";
 
     
     public static void main(String[] args) {
-        System.out.println(msg);
+        System.out.println(msg + msg1);
     }
 }
