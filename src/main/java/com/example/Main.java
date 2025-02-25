3b64cb4b9ada9a226d9d6b6866bf38684c115953
Nikita <47008746+pronin-ni@users.noreply.github.com>
2025-02-25 15:23:48 +0300

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class Main {
     private static String msg = "Hello, qa!!!!!";
+
+
     
     public static void main(String[] args) {
         System.out.println(msg);
